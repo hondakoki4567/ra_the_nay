283da5173c2801faa870586ce8e3c812c36ee469
hondakoki4567 <hondakoki4567@github.com>
2024-11-28 22:28:34 +0700

    up

---
+++ m-z-g-t-o-o-n.meta.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         m-z-g-t-o-o-n
+// @namespace    m-z-g-t-o-o-n.user.js
+// @version      2024.11.28
+// @description  down m-z-g-t-o-o-n
+// @author       You
+// @include      /^https?:\/\/z\d+\.mzgt[o0]on\.com\/webt[o0]on\/*/*/
+// @icon         https://raw.githubusercontent.com/hondakoki4567/ra_the_nay/main/m-z-g-t-o-o-n.png
+// @require      https://cdn.jsdelivr.net/npm/@zip.js/zip.js@2.7.53/dist/zip.min.js
+// @run-at       document-start
+// @downloadURL  https://github.com/hondakoki4567/ra_the_nay/raw/main/m-z-g-t-o-o-n.user.js
+// @updateURL    https://github.com/hondakoki4567/ra_the_nay/raw/main/m-z-g-t-o-o-n.meta.js
+// ==/UserScript==
